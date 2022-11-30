412539d7b62471338184c00b71ad7722d0798e6e
alanhakhyeonsong <songs4805@naver.com>
2022-11-30 10:11:58 +0900

    Feat: Like query 추가

--- jpa-board-hakhyeonsong/src/main/java/com/nhnacademy/board/repository/BoardRepository.java
+++ jpa-board-hakhyeonsong/src/main/java/com/nhnacademy/board/repository/BoardRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Integer> {
 
     @Transactional
     @Modifying
     @Query("update Board b set b.commentCount = :amount where b.boardId = :boardId")
     void updateBoard(@Param("boardId") Integer boardId, @Param("amount") Integer amount);
+
+    @Query("select b from Board b where b.title like %:title%")
+    List<Board> findAllByTitleLike(@Param("title") String title);
 }
--- jpa-board-hakhyeonsong/src/test/java/com/nhnacademy/board/repository/BoardRepositoryTest.java
+++ jpa-board-hakhyeonsong/src/test/java/com/nhnacademy/board/repository/BoardRepositoryTest.java
@@ -13,6 +13,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -41,5 +43,23 @@ class BoardRepositoryTest {
         Board board = boardRepository.findById(boardId).get();
 
         assertThat(board.getCommentCount()).isEqualTo(commentUpdateAmount);
+    }
+
+    @Test
+    @DisplayName("게시글 제목으로 검색 성공")
+    void findAllByTitleLike_success() throws Exception {
+        //given
+        String title = "게시";
+
+        //when
+        List<Board> boards = boardRepository.findAllByTitleLike(title);
+
+        //then
+        Board board = boards.get(0);
+
+        assertThat(board.getTitle()).isEqualTo("게시글1");
+        assertThat(board.getContent()).isEqualTo("게시글 내용");
+        assertThat(board.getCommentCount()).isZero();
+        assertThat(board.getEditorName()).isEqualTo("admin");
     }
 }
